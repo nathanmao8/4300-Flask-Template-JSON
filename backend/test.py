f360b8d787c23d73eea9766132552e344f3cdd97
nickspoto <nickspoto1221@gmail.com>
2025-03-21 22:27:36 -0400

    Finish sport search method and fix many preexisting errors

--- backend/test.py
+++ backend/test.py
@@ -1,3 +1,4 @@
+
 from collections import Counter
 import csv
 import json
@@ -9,25 +10,6 @@ import numpy as np
 from helpers.MySQLDatabaseHandler import MySQLDatabaseHandler
 import pandas as pd
 from nltk import TreebankWordTokenizer
-
-# ROOT_PATH for linking with all your files. 
-# Feel free to use a config.py or settings.py with a global export variable
-os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..",os.curdir))
-
-# Get the directory of the current script
-current_directory = os.path.dirname(os.path.abspath(__file__))
-
-# Specify the path to the JSON file relative to the current script
-json_file_path = os.path.join(current_directory, 'init.json')
-
-# Assuming your JSON data is stored in a file named 'init.json'
-with open(json_file_path, 'r') as file:
-    data = json.load(file)
-    episodes_df = pd.DataFrame(data['episodes'])
-    reviews_df = pd.DataFrame(data['reviews'])
-
-app = Flask(__name__)
-CORS(app)
 
 #function to convert 'megaGymDataset.csv' to 'exerciseDataset.json' 
 csv_filename = 'data/megaGymDataset.csv'
@@ -199,23 +181,7 @@ def sport_search(sport, query, num_exercises=30):
         exercises.append(top_k)
     return exercises
 
-# Sample search using json with pandas
-def json_search(query):
-    matches = []
-    merged_df = pd.merge(episodes_df, reviews_df, left_on='id', right_on='id', how='inner')
-    matches = merged_df[merged_df['title'].str.lower().str.contains(query.lower())]
-    matches_filtered = matches[['title', 'descr', 'imdb_rating']]
-    matches_filtered_json = matches_filtered.to_json(orient='records')
-    return matches_filtered_json
-
-@app.route("/")
-def home():
-    return render_template('base.html',title="sample html")
-
-@app.route("/episodes")
-def episodes_search():
-    text = request.args.get("title")
-    return json_search(text)
-
-if 'DB_NAME' not in os.environ:
-    app.run(debug=True,host="0.0.0.0",port=5000)
+exercises = sport_search("basketball", "I am a basketball player looking to build strength and athleticism through fast exercises.")
+for group in exercises:
+    for exercise in group:
+        print(exercise["Description"])
